e259a4b3d1d1535c4e75efc3e83f92aaed0ed040
davimbastos <davimbastos@gmail.com>
2022-01-05 16:14:21 -0300

    primeiro commit

---
+++ ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    internal class Elevador
+    {
+        
+        //Declaração das variáveis do projeto
+        int andarAtual;
+        int totalAndares;
+        int capElevador;
+        int qtdPessoas;
+
+
+        public void Inicializar(int andares, int capacidade) //Método para definir a quantidade de andares e a capacidade máxima.
+        {
+            totalAndares = andares;                     //Variável totalAndares recebe por parâmentro o número de andares 
+            capElevador = capacidade;                   //Variável capElevador recebe por parâmentro o máximo de pessoas
+            qtdPessoas = 0;                             //O elevador inicia com nenhuma pessoa  
+            andarAtual = 0;                             //O elevador inicia no térreo 
+            Console.WriteLine("Elevador Iniciado!");    //Messagem para notificar o usuário que o elevador foi iniciado
+        }
+
+        public void Entrar()                            //Método para adicionar pessoas ao elevador
+        {
+            if (qtdPessoas < capElevador)               //Compara o valor atual com a capacidade máxima
+            {
+                qtdPessoas++;                           //Adiciona uma pessoa se o elevador não estiver cheio
+                Console.WriteLine("Total de pessoas no elevador: " + qtdPessoas);   //Exibi a quantidade de pessoas
+            }
+            else
+            {
+                Console.WriteLine("O elevador está em sua capacidade máxima! Por favor, aguarde o próximo.");   //Notifica o usuário que o elevador está cheio
+            }
+        }
+
+        public void Sair()                              //Método para retirar pessoas do elevador
+        {
+            if (qtdPessoas > 0)                         //Verifica se o elevador não está vazio
+            {
+                qtdPessoas--;                           //Retira uma pessoa caso o elevador não esteja vazio
+                Console.WriteLine("Total de pessoas no elevador: " + qtdPessoas);   //Exibi a quantidade de pessoas
+            }
+            else
+            {
+                Console.WriteLine("O elevador está vazio!");        //Notifica o usuário que o elevador está vazio
+            }
+        }
+
+        public void Subir()                                         //Método para subir de andar
+        {
+            if (andarAtual < totalAndares)                          //Compara o andar atual com o total de andares
+            {
+                andarAtual++;                                       //Sobe um andar se o elevador não estiver no último
+                Console.WriteLine("Andar atual: " + andarAtual);    //Exibi o andar atual
+            }
+            else
+            {
+                Console.WriteLine("O elevador já está no último andar. Por favor, escolha uma andar abaixo."); //Notifica o usuário que o elevador chegou no último andar
+            }
+        }
+
+        public void Descer()                                        //Método para descer de andar
+        {
+            if (andarAtual > 0)                                     //Verifica se o elevador não está no térreo 
+            {
+                andarAtual--;                                       //Desce um andar
+                Console.WriteLine("Andar atual: " + andarAtual);    //Exibi o andar atual
+            }
+            else
+            {
+                Console.WriteLine("O elevador já está no térreo. Por favor, escolha uma andar acima.");     //Notifica o usuário que o elevador está no térreo.
+            }
+        }
+
+
+    }
+}
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,87 @@
+﻿using ProjetoElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevador elevador = new Elevador();                                 //Criação de um objeto do tipo Elevador
+
+            int cont = 0;                                                       // Inicia uma variável de controle
+                
+            
+            // Informações iniciais do projeto
+            Console.WriteLine("PROJETO ELEVADOR");
+            Console.WriteLine("");
+            Console.WriteLine("CONFIGURAÇÕES INICIAIS");
+            
+            Console.WriteLine("O elevador possui quantos andares?");            // Pede para o usuário definir o total de andares
+            int andares = int.Parse(Console.ReadLine());                        // Armazena o total de andares na variável andares
+            Console.WriteLine("Qual a capacidade máxima de pessoas?");          // Pede para o usuário definir a capacidade máxima
+            int capacidade = int.Parse(Console.ReadLine());                     // Armazena a capacidade máxima na variável capacidade
+
+            while (cont == 0)                                                   // Loop de controle do elevador
+            {
+                // Instruções sobre o funcionamento do elevador
+                Console.WriteLine("");
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("Digite o número para escolher uma das opções abaixo.");
+                Console.WriteLine("");
+                Console.WriteLine("Inicializar - 1");
+                Console.WriteLine("");
+                Console.WriteLine("Entrar - 2");
+                Console.WriteLine("Sair - 3");
+                Console.WriteLine("Subir - 4");
+                Console.WriteLine("Descer - 5");
+                Console.WriteLine("");
+                Console.WriteLine("Redefinir configurações do elevador - 6");
+                Console.WriteLine("Fechar Programa - 7");
+                Console.WriteLine("");
+                int opcao = int.Parse(Console.ReadLine());                      // Armazena a escolha no usuário na variavel opcao
+
+                switch (opcao)                                                  // Tratamento das opções escolhidas pelo usuário
+                {
+                    case 1:
+                        elevador.Inicializar(andares, capacidade);              // Chama o método Inicializar e envia os andares e a capacidade escolhida pelo usuário
+                        break;
+                    case 2:
+                        elevador.Entrar();                                      // Chama o método Entrar
+                        break;  
+                    case 3:
+                        elevador.Sair();                                        // Chama o método Entrar
+                        break;
+                    case 4:
+                        elevador.Subir();                                       // Chama o método Entrar
+                        break;
+                    case 5:
+                        elevador.Descer();                                      // Chama o método Entrar
+                        break;
+                    case 6:                                                     // Recebe as novas configurações do elevador 
+                        Console.WriteLine("O elevador possui quantos andares?");
+                        andares = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Qual a capacidade máxima de pessoas?");
+                        capacidade = int.Parse(Console.ReadLine());
+                        elevador.Inicializar(andares, capacidade);              // Chama o método Inicializar passando os novos valores
+                        break;
+                    case 7:
+                        cont = 1;                                               // Muda o contador para 1 e encerra o loop
+                        break;
+                    default:
+                        Console.WriteLine("Nenhuma opção válida selecionada!"); // Notifica o usuário caso nenhum dos valores válidos seja digitado
+                        break;
+                }
+
+            }
+
+            Console.WriteLine("Programa encerrado!");                           //Notifica o usuário que o programa terminou
+
+
+
+
+
+
+        }
+    }
+}
